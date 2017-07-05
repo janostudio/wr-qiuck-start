d5e3c7eb835fd1332a1ffc658895336676955ab9
jiang <qinghuangniao@gmail.com>
2017-07-06 07:15:08 +0800

    feat 为将redux-thunk替换为redux-saga做准备。

--- src/Components/main.jsx
+++ src/Components/main.jsx
@@ -2,6 +2,29 @@ import React, { Component } from "react";
 
 export default class Main extends Component {
   render() {
-    return <div>1Page not found</div>;
+    return (
+      <div className="verb">
+        <br />
+        <br />
+        <br />
+        <input
+          className="input-default"
+          type="text"
+          name="username"
+          placeholder="请输入您的用户名"
+        />
+        <br />
+        <input
+          className="input-default"
+          type="password"
+          name="password"
+          placeholder="请输入您的密码"
+        />
+        <br />
+        <button className="btn-default">login</button>
+        <br />
+        <button className="btn-default">loginout</button>
+      </div>
+    );
   }
 }
